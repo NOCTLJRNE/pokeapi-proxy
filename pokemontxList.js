3176226f4592ea7234495eee1d8575e796bd6495
LJnity <noctljrne@live.com>
2019-11-09 22:16:30 +0100

    1st commit

---
+++ pokemontxList.js
@@ -0,0 +1,5 @@
+export default {
+  "d8d83d73bed2acd693607076e4e097689c1de9f5c542dfdb177f459a6ee5154a": "Treecko",
+  "4cef23ac268b6193f18d4f5fa6c610e2ae80b37162675feae07fdffdbbe4cfdc": "Torchic",
+  "c4b49827550024f657c9b0e7327985a9d5f9a3053cc3174fd53428adb448e523": "Mudkip"
+};
